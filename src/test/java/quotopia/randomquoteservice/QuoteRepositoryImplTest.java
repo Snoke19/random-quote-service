a7dbd62c857c66dccb8231e0944800624f165963
Alex <alex.developer27@outlook.com>
2024-10-01 22:06:13 +0300

    renamed and added test with empty list parameter

--- src/test/java/quotopia/randomquoteservice/QuoteRepositoryImplTest.java
+++ src/test/java/quotopia/randomquoteservice/QuoteRepositoryImplTest.java
@@ -9,6 +9,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import quotopia.randomquoteservice.repository.impl.QuoteRepositoryImpl;
 
+import java.util.ArrayList;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -23,9 +25,17 @@ public class QuoteRepositoryImplTest {
     private QuoteRepositoryImpl quoteRepository;
 
     @Test
-    void test_illegal_argument_fetch_a_random_quote() {
+    void test_null_parameter_fetch_a_random_quote() {
         IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
                 () -> quoteRepository.findRandomQuoteByCategoriesExcludingPreviousId(null));
+
+        assertThat(illegalArgumentException.getMessage()).isEqualTo("Categories cannot be null or empty");
+    }
+
+    @Test
+    void test_empty_list_parameter_fetch_a_random_quote() {
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
+                () -> quoteRepository.findRandomQuoteByCategoriesExcludingPreviousId(new ArrayList<>()));
 
         assertThat(illegalArgumentException.getMessage()).isEqualTo("Categories cannot be null or empty");
     }
